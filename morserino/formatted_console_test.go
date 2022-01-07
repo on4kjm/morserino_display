f376f5aa1b89b3a8fd75c3f0b6a8ad4d718db8e1
Julien Levesy <jlevesy@gmail.com>
2022-01-07 19:21:10 +0100

    morserino/formatted_console: add a table driven test

--- morserino/formatted_console_test.go
+++ morserino/formatted_console_test.go
@@ -7,31 +7,57 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 )
 
 func TestFormattedConsole(t *testing.T) {
-	var (
-		output bytes.Buffer
-
-		ctx         = context.Background()
-		inputEvents = []Event{
-			{
-				Kind:    KindMessage,
-				Payload: []byte{'c'},
+	testCases := []struct {
+		desc       string
+		events     []Event
+		wantOutput string
+	}{
+		{
+			desc: "writes to output",
+			events: []Event{
+				{
+					Kind:    KindMessage,
+					Payload: []byte{'c'},
+				},
+				{Kind: KindEOF},
 			},
-			{Kind: KindEOF},
-		}
-
-		subject = NewFormattedConsole(&output)
-	)
-
-	for _, event := range inputEvents {
-		err := subject.Handle(event)
-		require.NoError(t, err)
+			wantOutput: "c",
+		},
 	}
 
-	err := subject.Run(ctx)
-	require.NoError(t, err)
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			var (
+				output bytes.Buffer
+				group  errgroup.Group
+				err    error
+			)
 
-	assert.Equal(t, "c", string(output.Bytes()))
+			// Initialize our console.
+			subject := NewFormattedConsole(&output)
+
+			// Run a goroutine to begin processing events while we're sending them.
+			// This is done to avoid blocking the eventQueue.
+			group.Go(func() error {
+				return subject.Run(context.Background())
+			})
+
+			// Send our events from the test case.
+			for _, event := range test.events {
+				err = subject.Handle(event)
+				require.NoError(t, err)
+			}
+
+			// Wait for our formatted console to process all the sent events.
+			err = group.Wait()
+			require.NoError(t, err)
+
+			// Assert that the output is what we actually want"
+			assert.Equal(t, test.wantOutput, string(output.Bytes()))
+		})
+	}
 }
